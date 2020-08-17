27e509e167687337db50094f1c3dda02db7ff8d6
tic20 <tic20@cam.ac.uk>
2020-08-17 18:31:37 +0100

    Removed method carried over from sample bundle.

--- src/cmd.py
+++ src/cmd.py
@@ -25,26 +25,3 @@ def phenix_get_restraints(session, ligand_id):
 phenix_get_restraints_desc = CmdDesc(
     required=[('ligand_id', StringArg)]
 )
-
-
-def sample_count(session, structures=None, api="python"):
-    if structures is None:
-        structures = session.models.list(type=AtomicStructure)
-    module_def = api_modules.get(api.lower(), None)
-    if module_def is None:
-        from chimerax.core.errors import UserError
-        err_string = ('Unrecognised API! Allowed choices are: {}').format(
-            ', '.join(api_modules.keys())
-        )
-    c_module, attribute = module_def
-    for m in structures:
-        if attribute is None:
-            arg = m
-        else:
-            arg = getattr(m, attribute)
-        atoms, bonds = c_module.counts(arg)
-        session.logger.info("%s: %d atoms, %d bonds" % (m, atoms, bonds))
-sample_count_desc = CmdDesc(
-    optional=[("structures", AtomicStructuresArg),],
-    keyword=[("api", StringArg),]
-)
